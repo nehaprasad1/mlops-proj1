63cc351346d1297405bb2e423f13ce637230a2f5
neha prasad <18.prasadneha@gmail.com>
2026-03-09 17:50:02 +0530

    logging and exception added

---
+++ demo.py
@@ -0,0 +1,9 @@
+from src.logger import logging
+#logging.debug("logging test !! this is a debug message..")
+#from src.exception import MyException
+#import sys
+#try:
+#    a = 1 + "o"
+#except Exception as e:
+#    logging.info(e)
+#    raise MyException(e,sys) from e
